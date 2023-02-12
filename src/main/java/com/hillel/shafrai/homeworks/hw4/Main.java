d15faff5d932754e57da22ff7919665e75568602
Julia Shafrai <yshafrai@gmsil.com>
2023-02-12 18:01:47 +0200

    add Main file

---
+++ src/main/java/com/hillel/shafrai/homeworks/hw4/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.shafrai.homeworks.hw4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("London eye coordinates");
+        int a = 51;
+        int b = 30;
+        char ch = '\u00B0';
+        System.out.println("DMS Lat: " + a + ch + " " + b + " ' 12.2364  N");
+        int y = 0;
+        int x = 7;
+        char cha = '\u00B0';
+        System.out.println("DMS Long: " + y+ cha + "  "+ x +" ' 10.2684  W");
+
+    }
+}
