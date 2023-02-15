51c9759390dc2a3f56855eb92b82e603ffbd72e2
Julia Shafrai <yshafrai@gmsil.com>
2023-02-15 21:31:20 +0200

    added

---
+++ src/main/java/com/hillel/shafrai/homeworks/hw5/Main.java
@@ -0,0 +1,19 @@
+package com.hillel.shafrai.homeworks.hw5;
+
+public class Main {
+    public static void main(String[] args) {
+        int warriorLi =13;
+        int archersLi = 24;
+        int riderLi = 46;
+
+        int warriorMin = 9;
+        int archersMin = 35;
+        int riderMin = 12;
+
+        int sumLi = (warriorLi+archersLi+riderLi)*860;
+        double sumMin = ((warriorMin+archersMin+riderMin)*860)*1.5;
+        System.out.println( "The general attacks of Lee's army ="+sumLi);
+        System.out.println( "The general attacks of the Min army ="+sumMin);
+
+    }
+}
