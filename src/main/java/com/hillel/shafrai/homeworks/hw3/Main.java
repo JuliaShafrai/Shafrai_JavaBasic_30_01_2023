8e5954a00b5d77e7e8eaf469747e9315d4f6b2b9
Julia Shafrai <yshafrai@gmsil.com>
2023-02-15 22:39:49 +0200

    added

---
+++ src/main/java/com/hillel/shafrai/homeworks/hw3/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.shafrai.homeworks.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Great Wall of China");
+        int a = 40;
+        int b = 26;
+        char ch = '°';
+        System.out.println("DMS Lat:"+a+ch+" "+b+"'16 N");
+        int y = 116;
+        int x = 33;
+        char cha = '°';
+        System.out.println("DMS Long:"+y+cha+" "+x+"'16 W");
+    }
+
+}
