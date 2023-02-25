1a9190e498809ecae84fab624c515ed75b5c42a7
Julia Shafrai <yshafrai@gmsil.com>
2023-02-25 19:37:42 +0200

    added

--- src/main/java/com/hillel/shafrai/homeworks/hw8/Main.java
+++ src/main/java/com/hillel/shafrai/homeworks/hw8/Main.java
@@ -3,19 +3,18 @@ package com.hillel.shafrai.homeworks.hw8;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Shuttle to Mars:");
-
         int counter=0;
         for ( int i = 1; i <= 100; i++) //noinspection SingleStatementInBlock
         {
 
-            if ((i / 10 == 4) || (i / 10 == 9)   || (i % 10 == 4) || (i % 10 == 9) ) {
+            if (i / 10 == 4 || i / 10 == 9   || i % 10 == 4 || i % 10 == 9) {
                 continue;
             }
             System.out.println("shuttle number = " + i);
             counter++;
         }
 
-        System.out.println("count shuttle = " + counter);
+        System.out.println("count lucky shuttle = " + counter);
 
 
 
